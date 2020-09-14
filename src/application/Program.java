a8205d01fac5e72a8770e331c4bd5dc758a46769
JorgeMartins15 <jorge_tcneletronica@hotmail.com>
2020-09-14 08:08:15 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(100, "Jorge");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(10, "Jorge", "martins@gmail.com", new Date(), 5000.00, obj);
+
+		System.out.println(seller);
 
 	}
 
