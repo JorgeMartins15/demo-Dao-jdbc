27a44f93301eedc5d6dfc53caf60271402aedd9b
JorgeMartins15 <jorge_tcneletronica@hotmail.com>
2020-09-14 07:29:21 -0300

    Department entities class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(100, "Jorge");
+		System.out.println(obj);
+
+	}
+
+}
